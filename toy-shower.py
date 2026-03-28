e3d98a7bbc3374cfa5a2b2796260fa89e93910f1
R4vante <l.teegelbeckers@hotmail.com>
2026-03-28 15:27:41 +0100

    part 4

--- toy-shower.py
+++ toy-shower.py
@@ -17,6 +17,28 @@ c_charge = 3
 num_events = np.arange(1, 10, 1)
 
 
+def get_rotation_matrix(parent_vec):
+    """Returns a matrix that rotates the z-unit vector [0, 0, 1] to align with the parent_vec"""
+    norm = np.linalg.norm(parent_vec)
+    if norm == 0:
+        return np.eye(3)
+
+    unit_p = parent_vec / norm
+    z_axis = np.array([0, 0, 1])
+
+    if np.allclose(unit_p, z_axis):
+        return np.eye(3)
+    if np.allclose(unit_p, -z_axis):
+        return -1 * np.eye(3)  # 180 deg flip
+
+    v = np.cross(z_axis, unit_p)
+    s = np.linalg.norm(v)
+    c = np.dot(z_axis, unit_p)
+    vx = np.array([[0, -v[2], v[1]], [v[2], 0, -v[0]], [-v[1], v[0], 0]])
+
+    return np.eye(3) + vx + np.dot(vx, vx) * ((1 - c) / (s**2))
+
+
 def main():
     # list of averages per event
     averages = []
@@ -26,9 +48,9 @@ def main():
 
         for iev in range(0, events):
             emissions_in_event = event(c_charge)
-            total_emissions += emissions_in_event
-
+            total_emissions += emissions_in_event[0]
         # calculate the average created number of particles
+
         average = total_emissions / events
 
         # append the averages to the list
@@ -48,10 +70,6 @@ def main():
 
     plt.show()
 
-    # Print the average number of created particles by averaging from the converged regime ~400 events
-    average_jets = np.mean(averages[int(400 / 50) :])
-    print(f"Average jets per event: {average_jets:.2f}")
-
 
 def event(c_charge):
     # counter to count the gluon jets
@@ -62,6 +80,12 @@ def event(c_charge):
     # part a: save the momenta
     pts_in_event = []
     z_values = []
+
+    # part 4:
+
+    parent_p4 = [ptHigh, 0, 0, ptHigh]  # Starting momentum parent
+    all_p4 = []  # Save momenta childs
+
     while True:
         # scale it by a random number
         sudakov *= random()
@@ -99,8 +123,31 @@ def event(c_charge):
         # sample z-values
         z_sample = np.random.choice(z_grid, p=p_norm)
         z_values.append(z_sample)
-    print(z_values)
-    return count
+
+        ## Part 4: rotation
+
+        phi = random() * 2 * np.pi  # sample random angle between  0 and 2*pi
+
+        E_p = parent_p4[0]
+        p1_loc = np.array(
+            [pt * np.cos(phi), pt * np.sin(phi), z_sample * E_p]
+        )  # local momentum first child
+        p2_loc = np.array(
+            [-pt * np.cos(phi), pt * np.sin(phi), (1 - z_sample) * E_p]
+        )  # local momentum second child
+
+        R = get_rotation_matrix(parent_p4[1:])  # get the rotation matrix
+
+        p1 = np.dot(R, p1_loc)  # rotated momentum p1
+        p2 = np.dot(R, p2_loc)  # rotated momentum p2
+
+        # Update the parent and samve emitted gluon
+        parent_p4 = np.array([np.linalg.norm(p1), *p1])
+        p2_p4 = np.array([np.linalg.norm(p2), *p2])
+
+        all_p4.append(p2_p4)
+
+    return count, all_p4
 
 
 def ptFromSudakov(sudakovValue, c_charge):
@@ -125,4 +172,5 @@ def ptFromSudakov(sudakovValue, c_charge):
     return pt
 
 
-main()
+if __name__ == "__main__":
+    main()
